191f9dd885cebd12d7eca4819ebae585906e7737
mbojarsk <mateusz.bojarski.stud@pw.edu.pl>
2022-12-18 00:59:24 +0100

    Initial commit with proprely formatted CSV file

---
+++ main.py
@@ -0,0 +1,6 @@
+def csv_readline(file):
+    pass
+
+while __name__ == "__main__":
+    pass
+    
---
+++ test_all.py
@@ -0,0 +1,6 @@
+# Placeholder tymczasowy
+# from main import *
+# from classes import *
+
+def test_tests():
+    assert True == True
