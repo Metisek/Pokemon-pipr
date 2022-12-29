f125979556af4e9e4637771585b80d702de54ed6
mbojarsk <mateusz.bojarski.stud@pw.edu.pl>
2022-12-29 19:04:12 +0100

    Implemented model_io read from JSON

--- test_classes.py
+++ test_classes.py
@@ -1,10 +1,10 @@
 from pytest import raises
 from classes import BasePokemon, GamePokemon
 from classes import (
-                     MalformedPokemonDataError,
                      PokemonDataDoesNotExistError,
                      BadConversionError,
-                     NotANumberError
+                     NotANumberError,
+                     #  MalformedPokemonDataError,
                     )
 import copy
 
@@ -144,6 +144,18 @@ def test_base_pokemon_get_special_vals_wrong_type():
         pokemon.get_special_strength_value('academic')
 
 
+def test_base_pokemon_empty_other_dict_vals_except_generation():
+    new_other = copy.deepcopy(other)
+    new_other['percentage_male'] = None
+    new_other['height_m'] = None
+    new_other['weight_kg'] = None
+    base_pokemon = BasePokemon(pokedex_number, name, abilities,
+                               stats, special_strength, new_other)
+    assert base_pokemon.get_other_value('height_m') is None
+    assert base_pokemon.get_other_value('weight_kg') is None
+    assert base_pokemon.get_other_value('percentage_male') is None
+
+
 def test_game_pokemon_init_typical():
     base_pokemon = BasePokemon(pokedex_number, name, abilities,
                                stats, special_strength, other)
@@ -174,7 +186,7 @@ def test_game_pokemon_init_random_other_values(monkeypatch):
                                stats, special_strength, other)
     game_pokemon = GamePokemon(base_pokemon)
     assert game_pokemon.get_max_hp() == 45
-    assert game_pokemon.get_gender() == 'Female'
+    assert game_pokemon.get_gender() == 'Male'
     assert game_pokemon.get_height() == 1.4
     assert game_pokemon.get_weight() == 13.8
 
@@ -322,3 +334,16 @@ def test_game_pokemon_set_defense_negative():
     game_pokemon = GamePokemon(base_pokemon, False)
     with raises(ValueError):
         game_pokemon.set_defense(-2)
+
+
+def test_game_pokemon_empty_other_dict_vals_except_generation():
+    new_other = copy.deepcopy(other)
+    new_other['percentage_male'] = None
+    new_other['height_m'] = None
+    new_other['weight_kg'] = None
+    base_pokemon = BasePokemon(pokedex_number, name, abilities,
+                               stats, special_strength, new_other)
+    game_pokemon = GamePokemon(base_pokemon)
+    assert game_pokemon.get_gender() == 'Unknown'
+    assert game_pokemon.get_height() is None
+    assert game_pokemon.get_weight() is None
